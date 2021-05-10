778730e431a882143ef3a6f3198248789ac1a069
Leo <ductien.tran@jobhopin.com>
2021-05-10 17:40:51 +0700

    second commit

---
+++ w3_how_to_practice_converter/weightConverter.js
@@ -0,0 +1,8 @@
+function weightConverterPoundsToKilograms(valNum) {
+    document.getElementById("outputKilograms").innerHTML=valNum/2.2046;
+}
+
+function weightConverterPoundsToOunces(valNum) {
+    document.getElementById("outputOunces").innerHTML=valNum*16;
+}
+
